24aba7a6a7d8953f5d9ee1de449f0167e5ff7700
Vansh Sameer <59049339+vsk-6926@users.noreply.github.com>
2022-09-11 05:23:58 +0530

    Update TopBar.jsx

--- client/src/components/topbar/TopBar.jsx
+++ client/src/components/topbar/TopBar.jsx
@@ -53,7 +53,7 @@ const Topbar = () => {
       <div className="topcenter">
         <ul className="topList">
           <li className="listItem">
-            <Link to="/">Home</Link>
+            <Link to="https://blognub.herokuapp.com/">Home</Link>
           </li>
           <li className="listItem">
             <Link to="">About</Link>
