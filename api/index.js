9c76a3334f35838bb6c9a0c8f988deb70d3f5bd0
Vansh Sameer <59049339+vsk-6926@users.noreply.github.com>
2022-09-11 05:37:53 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URL, {
